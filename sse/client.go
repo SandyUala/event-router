12a275f8b87ec8dbdfe51ba562c70f1c95ba7138
Ken Herner <ken@astronomer.io>
2017-10-20 18:57:33 -0400

    Fixed shutdown issues on SSE client
    Thanks Tyler!
    
    Signed-off-by: Ken Herner <ken@astronomer.io>

--- sse/client.go
+++ sse/client.go
@@ -5,6 +5,8 @@ import (
 	"os/signal"
 	"syscall"
 
+	"time"
+
 	"github.com/r3labs/sse"
 	"github.com/sirupsen/logrus"
 )
@@ -12,7 +14,9 @@ import (
 var log = logrus.WithField("package", "sse")
 
 type Client struct {
-	client *sse.Client
+	client         *sse.Client
+	shouldShutdown bool
+	shutdownChan   chan struct{}
 }
 
 func NewSSEClient(sseUrl string, authorization string) *Client {
@@ -33,15 +37,22 @@ func (c *Client) Subscribe(stream string, handler func(event []byte, data []byte
 	)
 
 	go func() {
-		run := true
-		for run {
-			run = c.listen(stream, handler)
-			if run {
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+		<-sigchan
+		c.shutdownChan <- struct{}{}
+		c.shouldShutdown = true
+	}()
+
+	go func() {
+		for {
+			if c.listen(stream, handler) && !c.shouldShutdown {
 				logger.Info("SSE Connection lost, reconnecting")
 			} else {
 				logger.Info("SSE Connection closed, exiting")
 				return
 			}
+			time.Sleep(time.Second * time.Duration(5))
 		}
 	}()
 }
@@ -50,23 +61,17 @@ func (c *Client) listen(stream string, handler func(event []byte, data []byte)) 
 	logger := log.WithField("function", "listen")
 	logger.Info("Starting SSE Listener")
 	eventChan := make(chan *sse.Event)
-	err := c.client.SubscribeChan(stream, eventChan)
-	if err != nil {
+	if err := c.client.SubscribeChan(stream, eventChan); err != nil {
 		logger.Error(err)
-		return false
+		return true
 	}
 	logger.Infof("Subscribed to SSE Stream %s", stream)
-	sigchan := make(chan os.Signal, 1)
-	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
 
-	run := true
 	event := sse.Event{}
-	for run {
+	for {
 		select {
-		case sig := <-sigchan:
-			logger.Infof("SSE Client caught signal %v: terminating", sig)
-			logger.Infof("Closing SSE Stream %s", stream)
-			run = false
+		case <-c.shutdownChan:
+			logger.Infof("SSE Client terminating...")
 			return false
 		case ev := <-eventChan:
 			if ev == nil {
@@ -79,21 +84,19 @@ func (c *Client) listen(stream string, handler func(event []byte, data []byte)) 
 				if string(ev.Event) == "heartbeat" {
 					logger.Debug("SSE Heartbeat")
 					event = sse.Event{}
-				} else {
-					event.Event = ev.Event
+					continue
 				}
+				event.Event = ev.Event
 			case len(ev.Data) != 0:
 				event.Data = ev.Data
-			}
-			// If we have data, send it up
-			if len(event.Data) != 0 {
 				handler(event.Event, event.Data)
 				logger.WithFields(logrus.Fields{
 					"event": string(event.Event),
 					"data":  string(event.Data),
 				}).Debug("Received Sent")
+				event = sse.Event{}
 			}
-			event = sse.Event{}
+
 		}
 	}
 	return true
