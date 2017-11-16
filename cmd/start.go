685ac1021e44b489f583762296a8ebb0c51105fd
Ken Herner <ken@astronomer.io>
2017-11-16 10:48:46 -0500

    Updating comments in start.go
    Signed-off-by: Ken Herner <ken@astronomer.io>

--- cmd/start.go
+++ cmd/start.go
@@ -60,6 +60,7 @@ func start(cmd *cobra.Command, args []string) {
 	logger := log.WithField("function", "start")
 	logger.Info("Starting event-router")
 
+	// Check if we are enabling Profiling or Tracing
 	if len(StartProfile) != 0 {
 		if StartProfile[len(StartProfile)-1] == '/' {
 			StartProfile = StartProfile[:len(StartProfile)-1]
@@ -75,7 +76,6 @@ func start(cmd *cobra.Command, args []string) {
 		}
 		defer pprof.StopCPUProfile()
 	}
-
 	if len(StartTrace) != 0 {
 		if StartTrace[len(StartTrace)-1] == '/' {
 			StartTrace = StartTrace[:len(StartTrace)-1]
@@ -93,6 +93,7 @@ func start(cmd *cobra.Command, args []string) {
 	}
 
 	// Create our simple web server
+	// It hosts the prometheus metrics and an endpoint that lists the current integrations in memory
 	apiClient := api.NewClient()
 	apiClient.AppendRouteHandler(v1.NewPromHandler())
 	// Setup api debug level (for gin logging)
@@ -103,6 +104,7 @@ func start(cmd *cobra.Command, args []string) {
 		config.SetBool(config.DisableCacheTTL, true)
 	}
 
+	// Grab some global config values
 	bootstrapServers := config.GetString(config.BootstrapServers)
 	topic := config.GetString(config.KafkaIngestionTopic)
 	config.SetBool(config.KafakDebug, KafkaDebug)
@@ -111,17 +113,17 @@ func start(cmd *cobra.Command, args []string) {
 	// Shutdown Channel
 	shutdownChannel := make(chan struct{})
 
-	// HTTP Client
+	// HTTP Client, passed to Houston client
 	httpClient := pkg.NewHTTPClient()
 
-	// Houston Client
+	// Houston Client - Used to make calls to Houston
 	houstonClient := houston.NewHoustonClient(httpClient, config.GetString(config.HoustonAPIURL))
 
-	// Integration Client
+	// Integration Client - Holds enabled integrations in memory
 	integrationClient := integrations.NewClient(houstonClient, shutdownChannel)
 	apiClient.AppendRouteHandler(v1.NewIntegrationsHandler(integrationClient))
 
-	// SSE Client
+	// SSE Client - Used to connect to the Houston SSE Server
 	if !DisableSSE {
 		sseClient, err := sse.NewSSEClient(config.GetString(config.SSEURL), houstonClient, shutdownChannel)
 		if err != nil {
@@ -132,7 +134,7 @@ func start(cmd *cobra.Command, args []string) {
 		sseClient.Subscribe("clickstream", integrationClient.SSEEventListener)
 	}
 
-	// Create our clickstreamProducer
+	// Clickstream Producer Options holds all the settings for the Kafka Producer
 	clickstreamProducerOptions := &clickstream.ProducerConfig{
 		BootstrapServers: bootstrapServers,
 		Integrations:     integrationClient,
@@ -146,6 +148,7 @@ func start(cmd *cobra.Command, args []string) {
 	}
 
 	// If we have retry enabled, create the DeadLetter Queue Client
+	// We do this after the clickstreamProducerOptions as we pass the client into it
 	if EnableRetry {
 		s3Client, err := s3.NewClient()
 		if err != nil {
@@ -161,13 +164,17 @@ func start(cmd *cobra.Command, args []string) {
 			}, s3Client)
 		clickstreamProducerOptions.DeadletterClient = dlc
 	}
+	// Create the producer object.  Its starts an internal event handler then waits
+	// for calls from the Consumer
 	clickstreamProducer, err := clickstream.NewProducer(clickstreamProducerOptions)
 	if err != nil {
 		logger.Error(err)
 		os.Exit(1)
 	}
 
-	// Clickstream Consumer
+	// Clickstream Consumer - This reads messages off of the main Kafka stream and sends the messages
+	// to the producer.  The producer is responsible for finding out where the messages go and
+	// producing them to the appropriate kafka stream
 	clickstreamConsumer, err := clickstream.NewConsumer(&clickstream.ConsumerOptions{
 		BootstrapServers: bootstrapServers,
 		GroupID:          config.GetString(config.KafkaGroupID),
